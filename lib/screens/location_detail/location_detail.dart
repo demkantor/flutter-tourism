6f5e0f19a8f1b26f0dadd4facf97acd0f0948cdf
demkantor <demkantor@gmail.com>
2020-06-08 10:30:20 -0500

    initial commit

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import './text_section.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tourism'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextSection(Colors.red),
+          TextSection(Colors.green),
+          TextSection(Colors.blue),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/location_detail/text_section.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget {
+  final Color _color;
+
+  TextSection(this._color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: _color),
+      child: Text('hi'),
+    );
+  }
+}
